ff5fb2220ba72f6b6a6003e762ea1affc8f4aa1b
root <root@localhost.localdomain>
2017-11-26 21:01:53 +0800

    删除现在不用的文件资源

--- EB-SAM9G45/04-MDK_Source/MDK4.01_Examples/23_usb_device_cdc_serial/download/SAM9G45_MDK_nandflash.bat
+++
@@ -1,3 +0,0 @@
-sam-ba.exe \usb\ARM0 AT91SAM9G45-EK at91sam9g45ekes_test_demo.tcl > logfile.log 2>&1
-notepad logfile.log
-
--- EB-SAM9G45/06-WinCE_Source/OSDesigns/AT91SAM9G45M10EK_DEMO/AT91SAM9G45M10EK_DEMO/Wince600/AT91SAM9G45M10EK_ARMV4I/OAK/MISC/cesysgen.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call %_PUBLICROOT%\cebase\oak\misc\cesysgen.bat %*
-goto :EOF
